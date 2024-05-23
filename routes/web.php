36085ef01645048536761c12317fcc2a02b66bf5
Sacha Guignard <sacha.guignard@caensup.fr>
2024-05-23 04:34:50 +0200

    modif car login pas encore mis en place

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\GitHubInfoController;
 use App\Http\Controllers\SkillController;
 use Illuminate\Support\Facades\Route;
@@ -8,11 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SkillController::class, 'showSkills']);
 Route::get('/api/github-info', [GitHubInfoController::class, 'fetchGitHubInfo']);
-Route::get('/skills/create', [SkillController::class, 'create'])->name('skills.create');
-Route::post('/skills', [SkillController::class, 'store'])->name('skills.store');
+//Route::get('/skills/create', [SkillController::class, 'create'])->name('skills.create');
+//Route::post('/skills', [SkillController::class, 'store'])->name('skills.store');
 
 
-Route::get('/skills', [SkillController::class, 'index'])->name('skills.index');
-Route::post('/skills/{skill}/update', [SkillController::class, 'update'])->name('skills.update');
-Route::put('/icons/{icon}', [SkillController::class, 'updateIcon'])->name('icons.update');
-Route::post('/contact', [ContactController::class, 'store'])->name('mail');
+//Route::get('/skills', [SkillController::class, 'index'])->name('skills.index');
+//Route::post('/skills/{skill}/update', [SkillController::class, 'update'])->name('skills.update');
+//Route::put('/icons/{icon}', [SkillController::class, 'updateIcon'])->name('icons.update');
+//Route::post('/contact', [ContactController::class, 'store'])->name('mail');
