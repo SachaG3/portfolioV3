10b731b90f6feb8aad2d296fa255786cbdbc92fe
Sacha Guignard <sacha.guignard@caensup.fr>
2024-08-11 01:05:00 +0200

    ajout des vue projets (et de la création) et des l'authentification

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -10,33 +10,18 @@ class ContactController extends Controller
 {
     public function store(Request $request)
     {
-        // Valider les champs du formulaire y compris le reCAPTCHA
-        $request->validate([
-            'first-name' => 'required|string|max:255',
-            'last-name' => 'required|string|max:255',
+        $validated = $request->validate([
+            'first-name' => 'required',
+            'last-name' => 'required',
             'email' => 'required|email',
-            'message' => 'required|string|min:10',
-            'g-recaptcha-response' => 'required',
+            'message' => 'required',
         ]);
-
-        // Vérifiez le token reCAPTCHA avec Google
-        $response = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
-            'secret' => env('RECAPTCHA_SECRET_KEY'),
-            'response' => $request->input('g-recaptcha-response'),
-            'remoteip' => $request->ip(),
-        ]);
-
-        $result = $response->json();
-
-        if (!$result['success'] || $result['score'] < 0.5) {
-            return back()->withErrors(['captcha' => 'La validation du captcha a échoué, veuillez réessayer.']);
-        }
 
         $data = [
-            'firstName' => $request['first-name'],
-            'lastName' => $request['last-name'],
-            'email' => $request['email'],
-            'content' => $request['message'],
+            'firstName' => $validated['first-name'],
+            'lastName' => $validated['last-name'],
+            'email' => $validated['email'],
+            'content' => $validated['message'],
         ];
 
         Mail::to("contact@sachaguignard.fr")->send(new ContactFormMail($data));
